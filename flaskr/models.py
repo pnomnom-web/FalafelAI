4bf26bc7755b576de8dc7c58b41e04bce7c55e8a
PaiktraNom <paiktranom@gmail.com>
2023-11-09 14:17:49 -0500

    Added Pet model

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,8 +1,21 @@
 from database import db
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key = True)
-    user = db.Column(db.String(30),unique=True, nullable=False)
-    auth = db.Column(db.String,unique=True, nullable=False)
+    userid = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(30),unique=True, nullable=False)
+    authid = db.Column(db.String,unique=True, nullable=False)
+
+class Pet(db.Model):
+    __tablename__ = 'pet'
+
+    petid = db.Column(db.Integer, primary_key = True)
+    userid = db.Column(db.Integer, db.ForeignKey('user.userid'))
+    petName = db.Column(db.String(30), nullable=False)
+    petVoice = db.Column(db.String(30), nullable=False)
+    openpng = db.Column(db.String)
+    closedpng = db.Column(db.String) # Both openPNG and closedPNG are both locations to where the PNG is stored.
+    responses = db.Column(db.String) # Responses will hold a json literal for all the responses.
+
+
